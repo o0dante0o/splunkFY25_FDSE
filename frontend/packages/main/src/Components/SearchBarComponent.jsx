e68d33b31746f04a740d07a5e240ffe7a786c602
o0dante0o <j.a@a-vision.co>
2024-08-06 01:29:21 -0600

    2024-08-06 | Search is working  (but without Typesense yet :( )

--- frontend/packages/main/src/Components/SearchBarComponent.jsx
+++ frontend/packages/main/src/Components/SearchBarComponent.jsx
@@ -1,36 +1,78 @@
-// src/Components/SearchBar.jsx
-import React, { useState, useContext } from 'react';
-import { GlobalContext } from '../GlobalState';
+import React, { useEffect, useRef, useState, useContext } from 'react';
 import Search from '@splunk/react-ui/Search';
+import { GlobalContext } from '../GlobalState'; // Asegúrate de importar tu contexto global
 
-const SearchBarComponent = () => {
-  const [query, setQuery] = useState('');
-  const { setSearchResults } = useContext(GlobalContext);
+function Loading() {
+    const { setSearchResults } = useContext(GlobalContext);
+    const [isLoading, setIsLoading] = useState(false);
+    const [options, setOptions] = useState([]);
+    const [value, setValue] = useState('');
+    const fetchOptions = useRef(null);
 
-  const handleSearch = async (event) => {
-    setQuery(event.target.value);
-    if (event.target.value.length > 2) {
-      try {
-        const response = await fetch(`https://ve0g3ekx8b.execute-api.us-east-1.amazonaws.com/dev/search?q=${event.target.value}`);
-        if (response.ok) {
-          const searchResults = await response.json();
-          setSearchResults(searchResults);
+    const handleFetch = async (searchValue = '') => {
+        setValue(searchValue);
+        if (searchValue.length > 2) {
+            setIsLoading(true);
+            try {
+                const response = await fetch(
+                    `https://ve0g3ekx8b.execute-api.us-east-1.amazonaws.com/dev/search?q=${searchValue}`
+                );
+                if (response.ok) {
+                    const searchResults = await response.json();
+                    if (fetchOptions.current) {
+                        // Check if the component is still mounted
+                        setOptions(searchResults);
+                        setSearchResults(searchResults);
+                        setIsLoading(false);
+                    }
+                } else {
+                    console.error('Error fetching search results:', response.statusText);
+                    setIsLoading(false);
+                }
+            } catch (error) {
+                console.error('Error fetching search results:', error);
+                setIsLoading(false);
+            }
         } else {
-          console.error('Error fetching search results:', response.statusText);
+            setOptions([]);
+            setSearchResults([]);
         }
-      } catch (error) {
-        console.error('Error fetching search results:', error);
-      }
-    } else {
-      setSearchResults([]);
-    }
-  };
+    };
 
-  return (
-    <div>
-      <Search inline onChange={handleSearch} value={query} />
-    </div>
-  );
-};
+    const generateOptions = () => {
+        if (isLoading) {
+            return null;
+        }
 
-export default SearchBarComponent;
+        return Array.isArray(options)
+            ? options.map((result) => <Search.Option value={result.title} key={result.id} />)
+            : [];
+    };
+
+    const handleChange = (e, { value: searchValue }) => {
+        handleFetch(searchValue);
+    };
+
+    useEffect(() => {
+        fetchOptions.current = true;
+        handleFetch();
+
+        return () => {
+            fetchOptions.current = false;
+        };
+    }, []);
+
+    const searchOptions = generateOptions();
+
+    return (
+        <div style={{ width: '300px' }}>
+            {' '}
+            {/* Ajusta el tamaño aquí */}
+            <Search value={value} inline onChange={handleChange} isLoadingOptions={isLoading}>
+                {searchOptions}
+            </Search>
+        </div>
+    );
+}
+
+export default Loading;
