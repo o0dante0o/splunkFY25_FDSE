087c2dafe8ba447fabd7e8a8721aee2d2ff83e00
o0dante0o <j.a@a-vision.co>
2024-08-16 20:10:50 -0600

    2024-08-16 | All the views are rendering the information correctly

--- frontend/packages/main-app/src/main/webapp/pages/start/Views/tablesView.jsx
+++ frontend/packages/main-app/src/main/webapp/pages/start/Views/tablesView.jsx
@@ -1,15 +1,39 @@
+// src/Views/overview.jsx
 import React, { useContext } from 'react';
 import { GlobalContext } from '@splunk/global-state'; // Asegúrate de que la ruta sea correcta
 
-const TablesView = () => {
-    const { state } = useContext(GlobalContext);
-    const { currentPath } = state;
+const Overview = () => {
+    const { state, searchResults } = useContext(GlobalContext);
+    const { list, currentPath } = state;
+
+    const dataToRender =
+        searchResults && Object.keys(searchResults).length > 0 ? searchResults : list;
+
+    const collectionsForDataInventory = ['lookups', 'fields', 'index', 'sources'];
+    const collectionsForOtherPaths = ['dashboards', 'apps', 'reports', 'alerts'];
+
+    const renderData = (data, collections) => {
+        return (
+            <div>
+                {collections.map(
+                    (key, index) =>
+                        data[key] && (
+                            <div key={index}>
+                                <strong>{key}:</strong> {JSON.stringify(data[key])}
+                            </div>
+                        )
+                )}
+            </div>
+        );
+    };
 
     return (
         <div>
-            <h1> {currentPath === '/data-inventory' ? 'Data Inventory' : 'Knowledge Objects'}</h1>
+            {currentPath === '/data-inventory'
+                ? renderData(dataToRender, collectionsForDataInventory)
+                : renderData(dataToRender, collectionsForOtherPaths)}
         </div>
     );
 };
 
-export default TablesView;
+export default Overview;
